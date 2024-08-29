756f0547e1b70e1ea28fb4fb97bee7d9443c05d9
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-08-29 12:01:20 -0600

    api doesn't want to work

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::get('/test', function () {
+  return response()->json(['message' => 'API is working']);
+});
